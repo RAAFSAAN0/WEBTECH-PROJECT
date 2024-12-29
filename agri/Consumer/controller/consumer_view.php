10c0930111ba61d36a1b691408392096dc2f5184
RAAFSAAN0 <rafsangaming0101@gmail.com>
2024-12-29 14:15:19 +0600

    added new

--- agri/Consumer/controller/consumer_view.php
+++ agri/Consumer/controller/consumer_view.php
@@ -57,6 +57,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['profile_image'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Consumer Profile</title>
+    <link rel="stylesheet" href="../asset/consumer_view.css">
 </head>
 <body>
     <h1>Consumer Profile</h1>
